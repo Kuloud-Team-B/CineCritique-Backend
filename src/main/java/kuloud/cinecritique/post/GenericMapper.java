bfc72276531cee348b6dc65f25d8545240374266
gunsookim <ccpc4896@naver.com>
2024-05-01 17:51:52 +0900

    feat: GenericMapper 인터페이스 추가 및 MapStruct 매핑 구성
    - Dto와 Entity 간의 변환을 위한 GenericMapper 인터페이스 정의
    - 단일 객체 및 리스트 변환 메서드 구현
    - Movie, Cinema, Goods, Member, Hashtags 필드에 대한 MapStruct 매핑 규칙 정의
    - Movie 객체를 Long 타입으로 매핑하는 map 메서드 추가

--- src/main/java/kuloud/cinecritique/post/GenericMapper.java
+++ src/main/java/kuloud/cinecritique/post/GenericMapper.java
@@ -1,5 +1,9 @@
 package kuloud.cinecritique.post;
 
+import kuloud.cinecritique.movie.entity.Movie;
+import kuloud.cinecritique.post.dto.PostResponseDto;
+import org.mapstruct.Mapping;
+
 import java.util.List;
 
 public interface GenericMapper<D, E> {
@@ -8,8 +12,16 @@ public interface GenericMapper<D, E> {
 
     E toEntity(D d);
 
-    List<D> toDtoList(List<E> entityList);
+    List<PostResponseDto> toDtoList(List<E> entityList);
 
     List<E> toEntityList(List<D> dtoList);
 
+    @Mapping(source = "movie.id", target = "movie") // Movie 객체의 ID를 Long 필드에 매핑
+    @Mapping(source = "cinema.id", target = "cinema") // Cinema 객체의 ID를 Long 필드에 매핑
+    @Mapping(source = "goods.id", target = "goods") // Goods 객체의 ID를 Long 필드에 매핑
+    @Mapping(source = "member.id", target = "memberId") // Member 객체의 ID를 Long 필드에 매핑
+    @Mapping(source = "hashtags", target = "hashtag") // Hashtag 객체를 처리할 방법 명시(아래 설명 참조)
+
+    // MapStruct가 Movie 객체를 Long 타입으로 매핑하는 방법을 정의
+    Long map(Movie movie);
 }
