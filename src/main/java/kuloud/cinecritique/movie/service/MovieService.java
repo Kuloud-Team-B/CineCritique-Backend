e2e321c730964fb963c32d7023652910efde09ee
hee_!J <heeej920@naver.com>
2024-05-01 14:21:15 +0900

    Feat : MovieService 영화 추천 기능 추가

--- src/main/java/kuloud/cinecritique/movie/service/MovieService.java
+++ src/main/java/kuloud/cinecritique/movie/service/MovieService.java
@@ -56,4 +56,51 @@ public class MovieService {
                 movie.getGenre().getName()
         )).collect(Collectors.toList());
     }
+
+    
+    /*영화 추천 로직
+    * 사용자의 영화 선택에 따라 가장 인기 있는 장르를 결정, 이를 기반으로 영화 추천  */
+
+    // 페이지네이션을 지원하는 영화 목록 검색
+    public ResponseEntity<?> retrieveMovies(Pageable pageable) {
+        Page<Movie> moviesPage = movieRepository.findAll(pageable);
+        return new ResponseEntity<>(moviesPage.map(MovieDto::new), HttpStatus.OK);
+    }
+
+    // 사용자의 영화 선택에 따른 장르를 집계
+    public HashMap<MovieGenre, Integer> getPickedGenres(RecommendationDto recommendationDto) {
+        HashMap<MovieGenre, Integer> pickedGenres = new HashMap<>();
+        recommendationDto.getPickedMovies().forEach(movieId -> {
+            Movie movie = movieRepository.findById(movieId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_MOVIE));
+
+            movie.getGenres().forEach(genre -> {
+                //pickedGenres.put(genre, pickedGenres.getOrDefault(genre, 0) + 1);
+                pickedGenres.merge(genre, 1, Integer::sum); // Merge를 이용해 카운트 증가
+            });
+        });
+
+        return pickedGenres;
+    }
+
+    // 집계된 장르에서 최상위 두 개를 선택
+    public Set<MovieGenre> selectTopGenres(HashMap<MovieGenre, Integer> genreCountMap) {
+        return genreCountMap.entrySet().stream()
+                .sorted(Map.Entry.<MovieGenre, Integer>comparingByValue().reversed())
+                .limit(2)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+    }
+
+    // 추천 영화 검색
+    public List<MovieDto> getRecommendation(RecommendationDto recommendationDto) {
+        Set<MovieGenre> topGenres = selectTopGenres(getPickedGenres(recommendationDto));
+        /*return movieRepository.findAllByGenresIn(topGenres).stream()
+                .map(MovieDto::new)
+                .collect(Collectors.toList());*/
+        return topGenres.stream()
+                .flatMap(genre -> movieRepository.findByGenre(genre).stream())
+                .map(MovieDto::new)
+                .collect(Collectors.toList());
+    }
 }
