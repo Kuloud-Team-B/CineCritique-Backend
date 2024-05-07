6498e0df61bcfa4720a9716231268bad00d2dac3
gunsookim <ccpc4896@naver.com>
2024-05-07 21:29:27 +0900

    Refact: Comment 엔티티 리팩토링 및 삭제 로직 추가
    - @DynamicInsert를 사용하여 SQL 생성 최적화.
    - NoArgsConstructor의 접근 수준을 PROTECTED로 변경하여 캡슐화 강화.
    - isDeleted 필드를 @ColumnDefault("FALSE")와 함께 추가하여 소프트 삭제 관리.
    - 부모와 자식 댓글의 관계를 추가하여 스레드 댓글 지원.
    - 설명이 더 명확한 업데이트 메소드로 리팩토링 (updateComment, updatePost, updateParent).

--- src/main/java/kuloud/cinecritique/comment/entity/Comment.java
+++ src/main/java/kuloud/cinecritique/comment/entity/Comment.java
@@ -4,16 +4,21 @@ import jakarta.persistence.*;
 import kuloud.cinecritique.common.entity.BaseTimeEntity;
 import kuloud.cinecritique.member.entity.Member;
 import kuloud.cinecritique.post.entity.Post;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
 @Builder
+@DynamicInsert
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "comment")
 public class Comment extends BaseTimeEntity {
 
@@ -28,12 +33,35 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "post_id")
     private Post post;
 
-    @ManyToOne
+    @ColumnDefault("FALSE")
+    @Column(nullable = false)
+    private Boolean isDeleted;
+
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    public void update(String content) {
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
+
+    public void updateComment(String content) {
         this.content = content;
+    }
+
+    public void updatePost(Post post) {
+        this.post = post;
+    }
+
+    public void updateParent(Comment comment) {
+        this.parent = comment;
+    }
+
+    public void changeIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
     }
 
 }
