e2c5ed03bb4aaf8ff099a6bdffca52343d43e177
gunsookim <ccpc4896@naver.com>
2024-05-08 20:48:43 +0900

    Refact: 에러코드 추가
    - comment에 대한 에러 알림 추가

--- src/main/java/kuloud/cinecritique/common/exception/ErrorCode.java
+++ src/main/java/kuloud/cinecritique/common/exception/ErrorCode.java
@@ -45,8 +45,10 @@ public enum ErrorCode {
     NOT_EXIST_MEMBER(BAD_REQUEST, "해당 사용자를 찾을 수 없습니다."),
     NOT_EXIST_EMAIL(BAD_REQUEST, "해당 이메일을 찾을 수 없습니다."),
     BAD_PARAMETER(BAD_REQUEST, "양식에 맞춰서 입력해주세요."),
-    INVALID_LOGIN(BAD_REQUEST, "로그인 정보가 맞지 않습니다.")
-    ;
+    INVALID_LOGIN(BAD_REQUEST, "로그인 정보가 맞지 않습니다."),
+
+    // comment
+    NOT_EXIST_COMMENT(BAD_REQUEST, "해당 댓글을 찾을 수 없습니다.");
     private final HttpStatus httpStatus;
     private final String errorMessage;
 }
